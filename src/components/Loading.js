4dee27535a31385e0b5cb1dd951be0c1a8040843
Femikul <showunmifemi7@gmail.com>
2023-06-26 01:05:02 -0700

    Added Loading UI

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+const Loading = () => {
+    return (
+        <h1 id="load" style={{
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          fontSize: '150%',
+          color: 'black',
+        }}>
+          Loading...
+        </h1>
+    );
+  };
+  
+  export default Loading;
+  
